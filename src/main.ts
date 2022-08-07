3c567c7f13911669d5d12ae667b4452b3834f7d6
kk <1213099875@qq.com>
2022-08-07 10:23:16 +0800

    每个组件单独打包构建，可以按需引入

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import * as Icons from '@element-plus/icons-vue'
 import {toLine} from './utils/index'
 import './mock'
 // import components from './components'
-// 从打包的lib文件夹中引入组件和样式
+// 从打包的lib文件夹中引入全部的组件和样式
 import components from '../lib/kk-ele-plus-components.es.js'
 import '../lib/style.css'
+// 从打包的lib文件夹中引入单个的组件和样式
+// import chooseIcon from '../lib/chooseIcon/index.es'
+// import ../lib/chooseIcon/style.css'
 
 const app = createApp(App)
 // 全局注册图标 牺牲一点性能 重新命名成以el-icon-xxx开头
@@ -18,5 +21,8 @@ for(let i in Icons){
   app.component(`el-icon-${toLine(i)}`,(Icons as any)[i])
   // console.log((Icons as any)[i]);
 }
-app.use(router).use(ElementPlus).use(components)
+app.use(router)
+.use(ElementPlus)
+// .use(chooseIcon)
+.use(components)
 app.mount('#app')
